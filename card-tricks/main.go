574fdf2536fdcc6ae5219ecb26b9875c301ad7bc
garveyshah <garveyshah1595@gmail.com>
2024-08-02 01:45:46 +0300

    added a line for testing Preapended function

--- card-tricks/main.go
+++ card-tricks/main.go
@@ -14,8 +14,8 @@ func main() {
 	}
 
 	slice := []int{}
-	index := cards.CustomAtoi(os.Args[2])
-	value := cards.CustomAtoi(os.Args[3])
+	// index := cards.CustomAtoi(os.Args[2])
+	// value := cards.CustomAtoi(os.Args[3])
 
 	for _, num := range strings.Split(os.Args[1], " ") {
 		slice = append(slice, cards.CustomAtoi(string(num)))
@@ -23,5 +23,6 @@ func main() {
 
 	// fmt.Println(cards.FavoriteCards(slice))
 	// fmt.Println(cards.GetItem(slice))
-	fmt.Println(cards.SetItem(slice, index, value))
+	// fmt.Println(cards.SetItem(slice, index, value))
+	fmt.Println(cards.PrependItems(slice, 5, 6, 7))
 }
